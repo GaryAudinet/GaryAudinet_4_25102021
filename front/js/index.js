7272e7f4ba2b1bbcdc2d71cbbbb4e72b22782772
GaryAudinet <audinet.gary@gmail.com>
2021-11-16 19:20:47 +0100

    lien entre page, details produit

--- front/js/index.js
+++ front/js/index.js
@@ -1,35 +1,35 @@
 // API
 
-function apiCall() {
-    fetch('http://localhost:3000/api/products')
-      .then((response) => response.json())
-      .then((value) => {
-        addProducts(value);
-        console.log('value via API', value);
-      })
-      .catch((error) => {
-        alert('Notre serveur de répond pas, veuillez revenir ultérieurement.');
-      });
+function apiCom() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => response.json())
+    .then((value) => {
+      addProduct(value);
+      console.log('value via API', value);
+    })
+    .catch((error) => {
+      alert('Le serveur de répond pas, veuillez patienter.');
+    });
   }
-  apiCall();
-  
+  apiCom();
 
-  // Fonction qui permet d'ajouter les elements attendu de chaque article
 
-  function addProducts(value) {
-    for (product of value) {
-      const article = `
-            <a href="./product.html?_id=${product._id}">
-            <article>
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-               <h3 class="productName">${product.name}</h3>
-               <p class="productDescription">${product.description}</p>
-            </article>
-            </a>
-          `;
-      document
-        .getElementById('items')
-        .insertAdjacentHTML('beforeend', article);
-    }
+// Fonction qui permet d'ajouter les elements attendu de chaque produit
+
+function addProduct(value) {
+  for (product of value) {
+    const article = `
+      <a href="./product.html?_id=${product._id}">
+      <article>
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+        <h3 class="productName">${product.name}</h3>
+        <p class="productDescription">${product.description}</p>
+      </article>
+      </a>
+    `;
+    document
+    .getElementById('items')
+    .insertAdjacentHTML('beforeend', article);
   }
+}
   
---
+++ front/js/product.js
@@ -0,0 +1,45 @@
+// Lien entre un produit des pages accueil/produit
+
+const search_params = new URLSearchParams(location.search);
+const productID = search_params.get('_id');
+
+
+// API - Recuperation de l'id d'un produit à afficher
+
+fetch(`http://localhost:3000/api/products/${productID}`)
+.then((response) => response.json())
+.then((value) => {
+addProduct(value);
+})
+.catch((error) => {
+alert('Le serveur de répond pas, veuillez patienter.');
+});
+
+
+// Fonction permettant d'afficher les détails d'un produit
+
+function addProduct(product) {
+    const productImg = document.querySelector(
+        'body > main > div > section > article > div.item__img'
+    );
+    const productDescription = document.getElementById('description');
+    const productPrice = document.getElementById('price');
+    const productName = document.getElementById('title');
+    const productColors = document.getElementById('colors');
+    productImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    productName.innerHTML = `<h1>${product.name}</h1>`;
+    productPrice.innerText = `${product.price}`;
+    productDescription.innerText = `${product.description}`;
+    for (index in product.colors) {
+        productColors.options[productColors.options.length] = new Option(
+        product.colors[index],
+        product.colors[index]
+        );
+    }
+}
+
+
+
+
+
+
